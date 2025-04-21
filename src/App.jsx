573c598dfcc0825fd11538c3706eca3a9232307a
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-04-21 19:09:31 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 const API_URL = "https://backend-eskimo.onrender.com/api";
 
 
+
 export default function AdminPanel() {
   const navigate = useNavigate();
 
