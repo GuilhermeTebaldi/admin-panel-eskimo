1a50ddbc07759a6889664514e9de1b91e88bbdde
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-04-29 12:19:30 -0300

    Atualizações no site

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "./index.css";
+import './index.css';
+
 
 import Pedidos from "./Pedidos";
 import App from "./App.jsx";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './site-efapi/**/*.{js,ts,jsx,tsx}'
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./site-efapi/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
