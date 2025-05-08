338991c01c5981d3bb74cf8f831ef54124491e17
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-05-08 18:23:28 -0300

    Conecta painel admin ao backend oficial com token

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_URL = "https://backend-eskimo.onrender.com/api";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: API_URL,
 });
 
 api.interceptors.request.use((config) => {
