57f22af5836e608927a782f4fb5ccdbc685a15de
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-09-17 21:08:09 +0200

    (  WhatsApp apos pagamento     )

--- src/App.jsx
+++ src/App.jsx
@@ -151,8 +151,8 @@ export default function AdminPanel() {
             <button type="button" onClick={() => navigate("/pedidos")} style={btnOutline}>✅ Ver Pedidos</button>
             <button type="button" onClick={() => navigate("/configuracoes")} style={btnOutline}>⚙️ Configurações de Entrega</button>
             <button type="button" onClick={() => navigate("/categorias")} style={btnOutline}>⚙️ Categorias </button>
-            <button type="button" onClick={() => navigate("/pagamentos")} style={btnOutline}>💳 Pagamentos por Loja</button>
-            <button type="button" onClick={() => navigate("/pagamentos#whatsapp")} style={btnOutline}>📲 WhatsApp da Loja</button>
+            
+            <button type="button" onClick={() => navigate("/pagamentos#whatsapp")} style={btnOutline}>📲Pagamentos e WhatsApp da Loja</button>
 
 
           </div>
